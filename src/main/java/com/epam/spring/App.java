ddb570e8668463ff79d35951077d97dffc72bfeb
Olga_Kramska <olga010585@gmail.com>
2016-07-29 18:53:15 +0300

    Rejected commit: Default

--- src/main/java/com/epam/spring/App.java
+++ src/main/java/com/epam/spring/App.java
@@ -3,14 +3,13 @@ package com.epam.spring;
 import com.epam.spring.beans.Client;
 import com.epam.spring.beans.Event;
 import com.epam.spring.loggers.EventLogger;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by Olga_Kramska on 7/27/2016.
  */
 public class App {
-    private static ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
     private Client client;
     private EventLogger logger;
 
@@ -20,17 +19,22 @@ public class App {
     }
 
     public static void main(String[] args) {
-
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
         App app = (App) context.getBean("app");
 
-        app.logEvent("Some event for user 1");
-        app.logEvent("Some event for user 2");
+        Event event1 = (Event) context.getBean("event");
+        event1.setMessage("Some event for user 1");
+        Event event2 = (Event) context.getBean("event");
+        event2.setMessage("Some event for user 2");
+
+        app.logEvent(event1);
+        app.logEvent(event2);
+        context.close();
     }
 
-    public void logEvent(String msg) {
-        String message = msg.replaceAll(String.valueOf(client.getId()), client.getFullName());
-        Event event = (Event) context.getBean("event");
+    public void logEvent(Event event) {
+        String message = event.getMessage().replaceAll(String.valueOf(client.getId()), client.getFullName());
         event.setMessage(message);
         logger.logEvent(event);
     }
